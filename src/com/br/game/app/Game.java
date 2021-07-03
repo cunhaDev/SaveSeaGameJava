4330cbf04f02f3e1dbeafdcd449b5aaceb050ce7
cunhaDev <dev.mateuscunha@gmail.com>
2021-07-03 17:46:06 -0400

    feat: refactor game

--- src/com/br/game/app/Game.java
+++ src/com/br/game/app/Game.java
@@ -1,90 +1,84 @@
 package com.br.game.app;
 
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.Timer;
-
-import com.br.game.app.player.Player;
+import javax.swing.JLabel;
 
 public class Game extends JFrame {
-
-	private Timer timer;
-	private Image fundo;
-	private Fase fase;
-	private Player player;
-	private Graphics2D grafico;
-
+	
+	private ImageIcon iconBackground = new ImageIcon(getClass().getResource("background.jpg"));
+	private ImageIcon iconPlayerParado = new ImageIcon(getClass().getResource("playerParado.gif"));
+	private ImageIcon iconPlayerMovendo = new ImageIcon(getClass().getResource("playerMovendo.gif"));
+	
+	private int posPlayerX = 140;
+	private int posPlayerY = 200;
+	
+	JLabel LBackground = new JLabel(iconBackground);
+	JLabel LPlayer = new JLabel(iconPlayerParado);
+	
+	public Game() {
+		editWindow();
+		editComponents();
+		moveplayer();
+	}
+	
+	public void moveplayer() {
+		addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				//Method is not necessary
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				LPlayer.setIcon(iconPlayerParado);
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				LPlayer.setIcon(iconPlayerMovendo);
+				if(e.getKeyCode() == 38) {
+					posPlayerY -= 10;
+				}
+				
+				if(e.getKeyCode() == 40) {
+					posPlayerY += 10;
+				}
+				
+				if(e.getKeyCode() == 37) {
+					posPlayerX -= 10;
+				}
+				
+				if(e.getKeyCode() == 39) {
+					posPlayerX += 10;
+				}
+				LPlayer.setBounds(posPlayerX, posPlayerX, 200, 200);
+			}
+		});
+	}
+	
+	public void editComponents() {
+		LBackground.setBounds(0, 0, 400, 400);
+		LPlayer.setBounds(posPlayerX, posPlayerY, 200, 200);
+	}
+	
+	public void editWindow() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(400,439);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setLayout(null);
+		add(LPlayer);
+		add(LBackground);
+	}
+	
 	public static void main(String[] args) {
 		new Game();
 	}
 
-	protected Game() {
-		Componentes();
-		initializar();
-	}
-
-	public void Componentes() {
-		setTitle("Save Sea");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(400, 455);
-		setLocationRelativeTo(null);
-		setResizable(false);
-		setVisible(true);
-	}
-
-	public void initializar() {
-		fase = new Fase();
-		add(fase);
-		ImageIcon referencia = new ImageIcon("res/fundoFase/background.jpg");
-		fundo = referencia.getImage();
-		player = new Player();
-		timer = new Timer(5, new Listener());
-		timer.start();
-	}
-
-	private class Listener implements ActionListener {
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			fase.repaint();
-		}
-	}
-
-	public class Fase extends JPanel {
-		protected static final int ALTURA = 400;
-		protected static final int LARGURA = 455;
-
-		protected Fase() {
-			setDoubleBuffered(true);
-		}
-
-		public void paint(Graphics g) {
-			grafico = (Graphics2D) g;
-			grafico.drawImage(fundo, 0, 0, null);
-			
-			grafico.drawImage(player.getImagem(),
-					player.getX(), player.getY(),
-					player.getX() + player.getLar(),
-					player.getY() + player.getAlt(),
-					1 + (player.getPos() * player.getLar()), 1,
-					1 + (player.getPos() * player.getLar()) + player.getLar(),
-					player.getAlt() + 1, null);
-			
-			g.dispose();
-		}
-
-		public int getLar() {
-			return LARGURA;
-		}
-
-		public int getAlt() {
-			return ALTURA;
-		}
-	}
 }
+
